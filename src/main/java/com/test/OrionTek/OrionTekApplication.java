2a5f4bd9e742086bdcd8421dadf087a948f3c87a
Jean Carlos George <jean18699@gmail.com>
2024-12-04 17:31:21 -0400

    Initial commit. Project creation

---
+++ src/main/java/com/test/OrionTek/OrionTekApplication.java
@@ -0,0 +1,13 @@
+package com.test.OrionTek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionTekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrionTekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/OrionTek/OrionTekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.OrionTek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionTekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
